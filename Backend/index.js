3254130093d0cb51c5e7dd561018f1e517c24cc7
er-dhruvv <dhruvsankaliya18@gmail.com>
2026-03-27 13:03:43 +0530

    remove one import of cors

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,6 @@ import marksRoutes from "./routes/marksRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-import cors from "cors";
 
 const allowedOrigins = [
   "http://localhost:5173",   // Vite local
